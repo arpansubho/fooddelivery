46d58bf4d44a9f3dac01526dace49fcfd56459e6
arpansubho <arpansubho123@gmail.com>
2022-03-16 17:28:33 +0530

    Food delivery

---
+++ app/src/main/java/com/example/multiscreenapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.multiscreenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String MSG = "com.example.multiscreenapp.ORDER";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void placeOrder(View view){
+        //handling click button
+        Intent intent = new Intent(this, OrderActivity.class);
+        EditText editText1 = findViewById(R.id.editText1);
+        EditText editText2 = findViewById(R.id.editText2);
+        EditText editText3 = findViewById(R.id.editText3);
+        String message = "Order of " + editText1.getText().toString()+", "+editText2.getText().toString()+" & "+editText3.getText().toString() + " has been placed successfully!!";
+        intent.putExtra(MSG, message);
+        startActivity(intent);
+    }
+}
